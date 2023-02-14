075ed6277d485708b394c5fb66ea00fe143ce6ef
harish6211 <125341201+harish6211@users.noreply.github.com>
2023-02-14 11:46:24 +0530

    Update simple.test.js

--- __tests__/simple.test.js
+++ __tests__/simple.test.js
@@ -1,7 +1,7 @@
-test('1 + 1 = 2', () => {
-  expect(1 + 1).toBe(3);
+test('1 - 1 = 0', () => {
+  expect(1 - 1).toBe(0);
 });
 
-test('1 + 2 = 3', () => {
-  expect(1 + 2).toBe(3);
+test('3 + 2 = 5', () => {
+  expect(3 + 2).toBe(5);
 });
